71f7de0c3d97aeb9c99fd7e4284bf38898d5810b
catal <catalin16mai@yahoo.com>
2023-09-07 20:25:26 +0300

    Initialized Service Classes

--- src/main/java/com/example/privatehospitalapp/service/PatientService.java
+++ src/main/java/com/example/privatehospitalapp/service/PatientService.java
@@ -1,4 +1,32 @@
 package com.example.privatehospitalapp.service;
 
+import com.example.privatehospitalapp.dao.PatientRepository;
+import com.example.privatehospitalapp.entity.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PatientService {
+
+    @Autowired
+    public PatientRepository patientRepository;
+
+    public Patient savePatient(Patient patient){
+        return patientRepository.save(patient);
+    }
+
+    public Patient searchPatientById(Long id){
+        Patient patient = patientRepository.findById(id).get();
+        return patient;
+    }
+
+    public List<Patient> getAllPatients(){
+        return patientRepository.findAll();
+    }
+
+    public void deletePatientById(Long id){
+        patientRepository.deleteById(id);
+    }
 }
--- src/main/java/com/example/privatehospitalapp/service/RentService.java
+++ src/main/java/com/example/privatehospitalapp/service/RentService.java
@@ -1,4 +1,32 @@
 package com.example.privatehospitalapp.service;
 
+import com.example.privatehospitalapp.dao.RentRepository;
+import com.example.privatehospitalapp.entity.Rent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RentService {
+
+    @Autowired
+    public RentRepository rentRepository;
+
+    public Rent saveRent(Rent rent){
+        return rentRepository.save(rent);
+    }
+
+    public Rent searchRentById(Long id){
+        Rent rent = rentRepository.findById(id).get();
+        return rent;
+    }
+
+    public List<Rent> getAllRents(){
+        return rentRepository.findAll();
+    }
+
+    public void deleteRentById(Long id){
+        rentRepository.deleteById(id);
+    }
 }
