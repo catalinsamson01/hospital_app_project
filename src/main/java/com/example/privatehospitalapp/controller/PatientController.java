59db80bdb680f6bfcb0a1ba7a99df305c46c6f0b
catal <catalin16mai@yahoo.com>
2023-09-08 17:10:59 +0300

    Did the controllers for DoctorController, PatientController, RentController and tried a html page for patients.html

--- src/main/java/com/example/privatehospitalapp/controller/PatientController.java
+++ src/main/java/com/example/privatehospitalapp/controller/PatientController.java
@@ -1,4 +1,48 @@
 package com.example.privatehospitalapp.controller;
 
+import com.example.privatehospitalapp.entity.Patient;
+import com.example.privatehospitalapp.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/patients")
 public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @PostMapping()
+    public String addPatient(@ModelAttribute Patient patient){
+        Patient savedPatient = patientService.savePatient(patient);
+        return "homePage";
+    }
+
+    @GetMapping()
+    public String showPatients(final ModelMap modelMap){
+        List<Patient> patients = patientService.getAllPatients();
+        modelMap.addAttribute("patients", patients);
+        return "Patients:";
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable Long id){
+        Patient patient = patientService.searchPatientById(id);
+        if(id==1) {
+            throw new RuntimeException("The patient with id " + id + " doesn' exist.");
+        }
+        return ResponseEntity.ok(patient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatientById(@PathVariable Long id){
+        patientService.deletePatientById(id);
+    }
+
+
 }
